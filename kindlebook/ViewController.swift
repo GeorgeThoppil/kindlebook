d47da6e2aa9fefa0c1e1e3801cd4ef94b396292d
GeorgeThoppil <georgethoppil2@gmail.com>
2018-10-24 21:51:13 -0400

    Initial Commit

---
+++ kindlebook/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  kindlebook
+//
+//  Created by George Thoppil on 2018-10-24.
+//  Copyright © 2018 George Thoppil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
