2a90d2ab580486d6888d886f982865072de9e14b
GeorgeThoppil <georgethoppil2@gmail.com>
2018-10-27 21:12:05 -0400

    Book cell with ns constraint applied

--- kindlebook/bookcell.swift
+++ kindlebook/bookcell.swift
@@ -13,6 +13,7 @@ class BookCell : UITableViewCell {
     let coverageImageView : UIImageView = {
         let imageView = UIImageView()
         imageView.backgroundColor = .red
+        imageView.translatesAutoresizingMaskIntoConstraints = false
         return imageView
     }()
     
@@ -20,11 +21,13 @@ class BookCell : UITableViewCell {
     let titleLabel : UILabel = {
         let label = UILabel()
         label.text = " blab blah"
+        label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
     
     let authorLabel : UILabel = {
         let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
         label.text = " blab author"
         return label
     }()
@@ -32,14 +35,24 @@ class BookCell : UITableViewCell {
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         
-        
-        coverageImageView.frame = CGRect(x: 8, y: 8, width: 50, height: 50)
-        titleLabel.frame = CGRect(x: 66, y: 20, width: 50, height: 20)
-        authorLabel.frame = CGRect(x: 66, y: 48, width: 200, height: 20)
-        
         addSubview(coverageImageView)
+        coverageImageView.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 8).isActive = true
+        coverageImageView.topAnchor.constraint(equalTo: self.topAnchor, constant: 8).isActive = true
+        coverageImageView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -8).isActive = true
+        coverageImageView.widthAnchor.constraint(equalToConstant: 50).isActive = true
+        
         addSubview(titleLabel)
+        titleLabel.leftAnchor.constraint(equalTo: coverageImageView.rightAnchor, constant: 8).isActive = true
+        titleLabel.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -8).isActive = true
+        titleLabel.heightAnchor.constraint(equalToConstant: 20).isActive = true
+        titleLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor, constant: -10).isActive = true
+        
         addSubview(authorLabel)
+        authorLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 4).isActive = true
+        authorLabel.leftAnchor.constraint(equalTo: coverageImageView.rightAnchor, constant: 8).isActive = true
+        authorLabel.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -8).isActive = true
+        authorLabel.heightAnchor.constraint(equalToConstant: 20).isActive = true
+        
     }
     
     required init?(coder aDecoder: NSCoder) {
